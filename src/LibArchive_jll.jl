97dd589048c41ccaf45c9166a5084c72c846b70b
jlbuild <juliabuildbot@gmail.com>
2025-12-03 15:55:42 +0000

    LibArchive_jll build 3.8.4+0

--- src/LibArchive_jll.jl
+++ src/LibArchive_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("LibArchive")
-JLLWrappers.@generate_main_file("LibArchive", UUID("1e303b3e-d4db-56ce-88c4-91e52606a1a8"))
+JLLWrappers.@generate_main_file("LibArchive", Base.UUID("1e303b3e-d4db-56ce-88c4-91e52606a1a8"))
 end  # module LibArchive_jll
